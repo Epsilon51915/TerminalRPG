130cb1712d9867e13bcbed2b74a1395eb7b4d995
Tyler Carroll <tylercarroll1242785.2@gmail.com>
2024-01-13 03:06:04 -0700

    Added text editor file + appendString() to all getlines (EXCLUDING NAME)

---
+++ textEditor.hpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <algorithm>
+
+// Lowercase text
+void toLowercase(std::string& text);
+std::string toLowercaseCopy(std::string text);
+
+// Trim whitespace
+void trim(std::string& str, const unsigned char ch);
+
+void appendString(std::string& str);
